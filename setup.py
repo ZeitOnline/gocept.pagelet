b06c1d5a623fe16f69af88a42376e97fad56d614
Christian Zagrodnick <cz@flyingcircus.io>
2007-09-26 14:01:38 +0000

    easy pagelet registration

---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import os.path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'gocept.pagelet',
+    version = "0.1",
+    author = "Christian Zagrodnick",
+    author_email = "cz@gocept.com",
+    description = "Extensions for zope.formlib",
+    long_description = file(os.path.join(os.path.dirname(__file__),
+                                         'src', 'gocept', 'pagelet',
+                                         'README.txt')).read(),
+    license = "ZPL 2.1",
+    url='http://pypi.python.org/pypi/gocept.pagelet',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+
+    include_package_data = True,
+    zip_safe = False,
+
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'z3c.template',
+        'z3c.pagelet',
+    ],
+    extras_require = dict(
+        test=['zope.testing'])
+    )
---
+++ src/gocept/pagelet/zcml.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2007 gocept gmbh & co. kg
+# See also LICENSE.txt
+# $Id$
+
+import zope.configuration.fields
+import zope.interface
+import zope.publisher.interfaces.browser
+
+import z3c.pagelet.zcml
+import z3c.template.zcml
+
+
+class IPageletDirective(z3c.pagelet.zcml.IPageletDirective):
+    """A directive to easiliy register a new pagelet with a layout tempalte.
+
+    """
+
+    class_ = zope.configuration.fields.GlobalObject(
+        title=u"Class",
+        description=u"A class that provides attributes used by the pagelet.",
+        required=False,
+        )
+
+    template = zope.configuration.fields.Path(
+        title=u'Layout template.',
+        description=u"Refers to a file containing a page template (should "
+                     "end in extension ``.pt`` or ``.html``).",
+        required=False,
+        )
+
+
+def pageletDirective(
+    _context, name, permission, class_=None, for_=zope.interface.Interface,
+    layer=zope.publisher.interfaces.browser.IDefaultBrowserLayer,
+    allowed_interface=None, allowed_attributes=None, template=None,
+    **kwargs):
+
+    if class_:
+        new_class = class_
+    else:
+        new_class = type('SimplePagelet', (object, ), {})
+
+    z3c.pagelet.zcml.pageletDirective(
+        _context, new_class, name, permission, for_, layer,
+        allowed_interface, allowed_attributes)
+
+    if template:
+        z3c.template.zcml.templateDirective(
+            _context, template, for_=new_class, layer=layer)
