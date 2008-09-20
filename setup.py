c977aeeb8b5b4c81ee431d0c8cddde2a93dc4a86
Michael Howitz <mh@gocept.com>
2008-09-20 11:53:00 +0000

    preparing for public release

--- setup.py
+++ setup.py
@@ -6,16 +6,21 @@ import os.path
 
 from setuptools import setup, find_packages
 
+def read(*rel_path):
+    base_path = os.path.dirname(__file__)
+    path = (base_path,) + tuple(rel_path)
+    return file(os.path.join(*path)).read()
 
 setup(
     name = 'gocept.pagelet',
     version = "0.1dev",
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
-    description = "Easier pagelet handling",
-    long_description = file(os.path.join(os.path.dirname(__file__),
-                                         'src', 'gocept', 'pagelet',
-                                         'README.txt')).read(),
+    description = "Easier z3c.pagelet handling",
+    long_description = (
+        read('src', 'gocept', 'pagelet', 'README.txt') + '\n\n' +
+        read('CHANGES.txt')
+        ),
     license = "ZPL 2.1",
     url='http://pypi.python.org/pypi/gocept.pagelet',
 
