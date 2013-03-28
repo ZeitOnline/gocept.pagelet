ad0a4068b71bebb56e1770c8af535773aefb6291
Michael Howitz <mh@gocept.com>
2013-03-28 10:12:40 +0100

    Embedded build status in long_description

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
-See src/gocept/pagelet/README.txt
+================
+ gocept.pagelet
+================
+
+Easier z3c.pagelet handling
+
+.. image:: https://builds.gocept.com/job/gocept.pagelet/badge/icon
+  :target: https://builds.gocept.com/job/gocept.pagelet/
+
+.. contents::
--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ import os.path
 
 from setuptools import setup, find_packages
 
-def read(*rel_path):
+def read(rel_path):
+    rel_path = rel_path.split('/')
     base_path = os.path.dirname(__file__)
     path = (base_path,) + tuple(rel_path)
     return file(os.path.join(*path)).read()
@@ -17,10 +18,10 @@ setup(
     author = "Christian Zagrodnick",
     author_email = "cz@gocept.com",
     description = "Easier z3c.pagelet handling",
-    long_description = (
-        read('src', 'gocept', 'pagelet', 'README.txt') + '\n\n' +
-        read('CHANGES.txt')
-        ),
+    long_description = '\n\n'.join([
+        read('README.txt'),
+        read('CHANGES.txt'),
+        read('src/gocept/pagelet/README.txt')]),
     license = "ZPL 2.1",
     url='http://pypi.python.org/pypi/gocept.pagelet',
     classifiers=[
