38afa92291f41fd48f24f0aa62b547a2cb9c0bef
Michael Howitz <mh@gocept.com>
2016-04-06 16:29:27 +0200

    PEP8 + PEP257

--- src/gocept/pagelet/zcml.py
+++ src/gocept/pagelet/zcml.py
@@ -1,49 +1,41 @@
-# Copyright (c) 2007 gocept gmbh & co. kg
-# See also LICENSE.txt
-# $Id$
-
+from zope.configuration.exceptions import ConfigurationError
+import gocept.pagelet.viewletpage
+import z3c.pagelet.browser
+import z3c.pagelet.interfaces
+import z3c.pagelet.zcml
+import z3c.template.zcml
+import zope.browsermenu.field
+import zope.browserpage.metaconfigure
 import zope.configuration.fields
 import zope.interface
 import zope.publisher.interfaces.browser
 import zope.viewlet.metaconfigure
 
-import zope.browsermenu.field
-import zope.browserpage.metaconfigure
-
-import z3c.pagelet.browser
-import z3c.pagelet.interfaces
-import z3c.pagelet.zcml
-import z3c.template.zcml
-
-import gocept.pagelet.viewletpage
-
 
 class IPageletDirective(z3c.pagelet.zcml.IPageletDirective):
-    """A directive to easiliy register a new pagelet with a layout tempalte.
-
-    """
+    """A directive to easily register a new pagelet with a layout template."""
 
     for_ = zope.configuration.fields.Tokens(
         title=u"Specifications of the objects to be viewed",
         description=u"""This should be a list of interfaces or classes""",
         required=True,
         value_type=zope.configuration.fields.GlobalObject(
-          missing_value=object(),
-          ),
-        )
+              missing_value=object(),
+        ),
+    )
 
     class_ = zope.configuration.fields.GlobalObject(
         title=u"Class",
         description=u"A class that provides attributes used by the pagelet.",
         required=False,
-        )
+    )
 
     template = zope.configuration.fields.Path(
         title=u'Layout template.',
         description=u"Refers to a file containing a page template (should "
-                     "end in extension ``.pt`` or ``.html``).",
+        "end in extension ``.pt`` or ``.html``).",
         required=False,
-        )
+    )
 
     title = zope.configuration.fields.MessageID(
         title=u"The browser menu label for the page (view)",
@@ -52,7 +44,7 @@ class IPageletDirective(z3c.pagelet.zcml.IPageletDirective):
           supplied.
           """,
         required=False
-        )
+    )
 
     menu = zope.browsermenu.field.MenuField(
         title=u"The browser menu to include the page (view) in.",
@@ -64,15 +56,16 @@ class IPageletDirective(z3c.pagelet.zcml.IPageletDirective):
           </description>
           """,
         required=False
-        )
+    )
 
-def pageletDirective(
-    _context, name, permission, class_=None, for_=zope.interface.Interface,
-    layer=zope.publisher.interfaces.browser.IDefaultBrowserLayer,
-    allowed_interface=None, allowed_attributes=None, template=None,
-    title=None, menu=None,
-    **kwargs):
 
+def pageletDirective(  # noqa
+        _context, name, permission, class_=None, for_=zope.interface.Interface,
+        layer=zope.publisher.interfaces.browser.IDefaultBrowserLayer,
+        allowed_interface=None, allowed_attributes=None, template=None,
+        title=None, menu=None,
+        **kwargs):
+    """Register a new pagelet with a layout template."""
     if class_:
         new_class = class_
     else:
@@ -96,6 +89,7 @@ def pageletDirective(
 
 
 class ViewletPageDirective(object):
+    """Directive to register a new pagelet with a layout template."""
 
     def __init__(self, _context, name, permission,
                  class_=gocept.pagelet.viewletpage.ViewletPage,
@@ -119,13 +113,12 @@ class ViewletPageDirective(object):
             **kwargs)
 
 
-# pagelet directive
-def original_pageletDirective(
-    _context, class_, name, permission, for_=(zope.interface.Interface,),
-    layer=zope.publisher.interfaces.browser.IDefaultBrowserLayer,
-    provides=z3c.pagelet.interfaces.IPagelet,
-    allowed_interface=None, allowed_attributes=None, **kwargs):
-
+def original_pageletDirective(  # noqa
+        _context, class_, name, permission, for_=(zope.interface.Interface,),
+        layer=zope.publisher.interfaces.browser.IDefaultBrowserLayer,
+        provides=z3c.pagelet.interfaces.IPagelet,
+        allowed_interface=None, allowed_attributes=None, **kwargs):
+    """The original pagelet directive."""
     # Security map dictionary
     required = {}
 
@@ -149,7 +142,8 @@ def original_pageletDirective(
     cdict = {}
     cdict['__name__'] = name
     cdict.update(kwargs)
-    new_class = type(class_.__name__, (class_, z3c.pagelet.browser.BrowserPagelet), cdict)
+    new_class = type(
+        class_.__name__, (class_, z3c.pagelet.browser.BrowserPagelet), cdict)
 
     # Set up permission mapping for various accessible attributes
     zope.browserpage.metaconfigure._handle_allowed_interface(
@@ -171,12 +165,12 @@ def original_pageletDirective(
         zope.interface.classImplements(new_class, provides)
 
     # Create the security checker for the new class
-    zope.security.checker.defineChecker(new_class,
-        zope.security.checker.Checker(required))
+    zope.security.checker.defineChecker(
+        new_class, zope.security.checker.Checker(required))
 
     # register pagelet
     _context.action(
-        discriminator = ('pagelet',) +  for_ + (layer, name),
-        callable = zope.component.zcml.handler,
-        args = ('registerAdapter',
-                new_class, for_ + (layer,), provides, name, _context.info),)
+        discriminator=('pagelet',) + for_ + (layer, name),
+        callable=zope.component.zcml.handler,
+        args=('registerAdapter',
+              new_class, for_ + (layer,), provides, name, _context.info),)
