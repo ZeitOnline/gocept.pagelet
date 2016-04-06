e2baa742d529705249a279abb37240bd09f519c1
Michael Howitz <mh@gocept.com>
2016-04-06 16:47:03 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(rel_path):
 
 setup(
     name='gocept.pagelet',
-    version='1.0',
+    version='1.1.dev0',
     author="Christian Zagrodnick",
     author_email="mail@gocept.com",
     description="Easier z3c.pagelet handling",
