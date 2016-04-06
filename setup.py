db2ee978dda8802c78af180d4b11115ab2c1060a
Michael Howitz <mh@gocept.com>
2016-04-06 16:31:40 +0200

    PEP-8

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-# Copyright (c) 2007 gocept gmbh & co. kg
-# See also LICENSE.txt
-# $Id$
-
 import os.path
-
 from setuptools import setup, find_packages
+
 
 def read(rel_path):
     rel_path = rel_path.split('/')
@@ -13,16 +9,16 @@ def read(rel_path):
     return file(os.path.join(*path)).read()
 
 setup(
-    name = 'gocept.pagelet',
+    name='gocept.pagelet',
     version='0.5.dev0',
-    author = "Christian Zagrodnick",
-    author_email = "cz@gocept.com",
-    description = "Easier z3c.pagelet handling",
-    long_description = '\n\n'.join([
+    author="Christian Zagrodnick",
+    author_email="cz@gocept.com",
+    description="Easier z3c.pagelet handling",
+    long_description='\n\n'.join([
         read('README.txt'),
         read('CHANGES.txt'),
         read('src/gocept/pagelet/README.txt')]),
-    license = "ZPL 2.1",
+    license="ZPL 2.1",
     url='http://pypi.python.org/pypi/gocept.pagelet',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -34,16 +30,13 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'],
-    keywords = 'easy z3.pagelet zope3 pagelet zope',
-
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-
-    include_package_data = True,
-    zip_safe = False,
-
-    namespace_packages = ['gocept'],
-    install_requires = [
+    keywords='easy z3.pagelet zope3 pagelet zope',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=['gocept'],
+    install_requires=[
         'setuptools',
         'zope.interface',
         'zope.component',
@@ -53,7 +46,7 @@ setup(
         'z3c.pagelet',
         'zope.browsermenu',
         'zope.browserpage',
-        ],
-    extras_require = dict(
+    ],
+    extras_require=dict(
         test=['zope.testing'])
-    )
+)
