9b6550d206b73fbb706a4fd6af354fb577b3aad6
Michael Howitz <mh@gocept.com>
2019-06-10 07:18:12 +0200

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(rel_path):
 
 setup(
     name='gocept.pagelet',
-    version='1.1.dev0',
+    version='1.1',
     author="Christian Zagrodnick",
     author_email="mail@gocept.com",
     description="Easier z3c.pagelet handling",
